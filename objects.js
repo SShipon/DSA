106d8b93bb4a75627bd95e6100972a7079b23a52
SShipon <nxshipon@gmail.com>
2023-02-06 10:32:10 +0600

    user add

---
+++ objects.js
@@ -0,0 +1,25 @@
+const computer = {
+    brand:'Hp 250',
+    color:'black',
+    ram:'8gb',
+    input:'Italy',
+    core:'i7 10gen'
+}
+const computer2 = {
+    brand:'Lenovo',
+    color:'white',
+    ram:'8gb',
+    input:'America',
+    core:'i7 10gen'
+}
+const computer3 = {
+    brand:'dell',
+    color:'silver',
+    ram:'12gb',
+    input:'Us',
+    core:'i7 10gen'
+}
+
+const myLaptop =[computer, computer2,computer3]
+
+console.log(myLaptop);
